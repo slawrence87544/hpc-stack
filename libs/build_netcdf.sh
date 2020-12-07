b7ded76d76a804cd4df4338c5a0597ea3583ab26
Steve Lawrence <slawrence@APSDs-MacBook-Pro.local>
2020-12-06 22:57:48 -0700

    fix: incorrect flags to enable/disable pnetcdf or netcdf_cxx build
    body: although there is logic at the beginning of build_netcdf.sh
    to enable/disable both pnetcdf and build of netcdf_cxx, it sets
    them to either "YES" or "NO".  However, the checks for those
    flags later on is expecting them to be of the format yYtT, no
    YES.

--- libs/build_netcdf.sh
+++ libs/build_netcdf.sh
@@ -11,8 +11,8 @@ cxx_version=${3:-${STACK_netcdf_version_cxx}}
 compiler=$(echo $HPC_COMPILER | sed 's/\//-/g')
 mpi=$(echo $HPC_MPI | sed 's/\//-/g')
 
-[[ ${STACK_netcdf_enable_pnetcdf:-} =~ [yYtT] ]] && enable_pnetcdf=YES || enable_pnetcdf=NO
-[[ ${STACK_netcdf_disable_cxx:-} =~ [yYtT]  ]] && enable_cxx=NO || enable_cxx=YES
+[[ ${STACK_netcdf_enable_pnetcdf:-} =~ [yYtT] ]] && enable_pnetcdf=Y || enable_pnetcdf=N
+[[ ${STACK_netcdf_disable_cxx:-} =~ [yYtT]  ]] && enable_cxx=N || enable_cxx=Y
 
 if $MODULES; then
     set +x
