dbacbe19962fa305410efb4f813fbbee2d7d196e
Steve Lawrence <slawrence@APSDs-MacBook-Pro.local>
2020-12-07 15:32:03 -0700

    fix: check for disable_cxx always prevents compile of netcdf c++
    body: The check of the disable_cxx config setting has faulty
    syntax and always results in absence of compile of the netcdf c++
    library.  The syntax error prevents the check from allowing the
    compile even if the disable_cxx is set to NO.

--- libs/build_netcdf.sh
+++ libs/build_netcdf.sh
@@ -11,8 +11,8 @@ cxx_version=${3:-${STACK_netcdf_version_cxx}}
 compiler=$(echo $HPC_COMPILER | sed 's/\//-/g')
 mpi=$(echo $HPC_MPI | sed 's/\//-/g')
 
-[[ ${STACK_netcdf_enable_pnetcdf:-} =~ [yYtT] ]] && enable_pnetcdf=Y || enable_pnetcdf=N
-[[ ${STACK_netcdf_disable_cxx:-} =~ [yYtT]  ]] && enable_cxx=N || enable_cxx=Y
+[[ ${STACK_netcdf_enable_pnetcdf:-} =~ [yYtT] ]] && enable_pnetcdf=YES || enable_pnetcdf=NO
+[[ ${STACK_netcdf_disable_cxx:-} =~ [yYtT]  ]] && enable_cxx=NO || enable_cxx=YES
 
 if $MODULES; then
     set +x
@@ -177,7 +177,7 @@ $SUDO make install
 
 $MODULES || echo $software >> ${HPC_STACK_ROOT}/hpc-stack-contents.log
 
-if [ $enable_cxx =~ [yYtT] ]; then 
+if [[ ${enable_cxx} =~ [yYtT] ]]; then 
    
   cd $curr_dir
 
