bef07f015062477b0c1af922dff37169f07beb65
Steve Lawrence <slawrence@APSDs-MacBook-Pro.local>
2020-12-16 15:29:36 -0700

    feat: attempt to compile gsi with generic linux-based hpc-stack
    body: although I was successfully able to compile the gsi library
    using the Generic platform, I am encountering many warnings.  This
    custom hpc-stack build is an attempt to fix those compile errors.

--- libs/build_netcdf.sh
+++ libs/build_netcdf.sh
@@ -59,10 +59,11 @@ gitURLroot="https://github.com/Unidata"
 cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}
 curr_dir=$(pwd)
 
-LDFLAGS1="-L$HDF5_ROOT/lib -lhdf5_hl -lhdf5"
+LDFLAGS1="-L$HDF5_ROOT/lib"
 LDFLAGS2=$(cat $HDF5_ROOT/lib/libhdf5.settings | grep AM_LDFLAGS | cut -d: -f2)
 [[ $enable_pnetcdf =~ [yYtT] ]] && LDFLAGS4="-L$PNETCDF_ROOT/lib"
 if [[ ${STACK_netcdf_shared:-} != [yYtT] ]]; then
+  LDFLAGS1+="-lhdf5 -lhdf5_hl -lz"
   LDFLAGS3=$(cat $HDF5_ROOT/lib/libhdf5.settings | grep "Extra libraries" | cut -d: -f2)
   [[ $enable_pnetcdf =~ [yYtT] ]] && LDFLAGS4+=" -lpnetcdf"
 fi
@@ -109,7 +110,7 @@ software=$name-"c"-$version
 [[ -d build ]] && rm -rf build
 mkdir -p build && cd build
 
-[[ ${STACK_netcdf_shared} =~ [yYtT] ]] || shared_flags="--disable-shared"
+[[ ${STACK_netcdf_shared} =~ [yYtT] ]] shared_flags="--enable-shared --enable-static" || shared_flags="--disable-shared"
 [[ $enable_pnetcdf =~ [yYtT] ]] && pnetcdf_conf="--enable-pnetcdf"
 [[ -z $mpi ]] || extra_conf="--enable-parallel-tests"
 
@@ -118,6 +119,8 @@ mkdir -p build && cd build
              --disable-dap \
              --enable-netcdf-4 \
              --disable-doxygen \
+             --enable-parallel4 \
+             --disable-large-file-tests \
              ${shared_flags:-} ${pnetcdf_conf:-} ${extra_conf:-}
 
 VERBOSE=$MAKE_VERBOSE make -j${NTHREADS:-4}
@@ -149,10 +152,10 @@ set -x
 
 if [[ ${STACK_netcdf_shared} =~ [yYtT] ]]; then
   export LIBS=$($prefix/bin/nc-config --libs)
-  export LDFLAGS+=" -L$prefix/lib"
+  export LDFLAGS+=" -L$prefix/lib -lnetcdf -lhdf5_hl -lhdf5 -lz"
 else
   export LIBS=$($prefix/bin/nc-config --libs --static)
-  export LDFLAGS+=" -L$prefix/lib -lnetcdf -lhdf5"
+  export LDFLAGS+=" -L$prefix/lib -lnetcdf -lhdf5_hl -lhdf5 "
 fi
 export CFLAGS+=" -I$prefix/include"
 export CXXFLAGS+=" -I$prefix/include"
