113606e2fc10cb25249923cbe4a4f7c3f8b6dc95
slawrence87544 <57503604+slawrence87544@users.noreply.github.com>
2021-02-03 19:32:33 -0700

    fix: problem with hdf5 build, reverting to emc version from develop

--- libs/build_hdf5.sh
+++ libs/build_hdf5.sh
@@ -60,9 +60,9 @@ software=$name-$(echo $version | sed 's/\./_/g')
 [[ -d build ]] && rm -rf build
 mkdir -p build && cd build
 
-[[ -z $mpi ]] || extra_conf="--enable-parallel --enable-unsupported --enable-build-mode=production --enable-static"
+[[ -z $mpi ]] || extra_conf="--enable-parallel --enable-unsupported"
 
-[[ $enable_shared =~ [yYtT] ]] && shared_flags="--enable-shared" || shared_flags="--disable-shared --enable-static-exec"
+[[ $enable_shared =~ [yYtT] ]] || shared_flags="--disable-shared --enable-static --enable-static-exec"
 [[ $enable_szip =~ [yYtT] ]] && szip_flags="--with-szlib=$SZIP_ROOT"
 [[ $enable_zlib =~ [yYtT] ]] && zlib_flags="--with-zlib=$ZLIB_ROOT"
 
